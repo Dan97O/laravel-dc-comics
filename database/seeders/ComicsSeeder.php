13a4e1b20dc522e5bfa55c88fc66b8653ec9bf75
Dan97O <dan97-oddo@live.it>
2023-05-29 15:17:50 +0200

    add seeder

--- database/seeders/ComicsSeeder.php
+++ database/seeders/ComicsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comics;
 use Illuminate\Database\Seeder;
 
 class ComicsSeeder extends Seeder
@@ -13,6 +14,19 @@ class ComicsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
+        foreach ($comics as $comic) {
+            $newComic = new Comics();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->artists = $comic['artists'];
+            $newComic->writers = $comic['writers'];
+            $newComic->save();
+        }
     }
 }
